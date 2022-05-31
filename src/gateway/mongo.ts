dfb8b5cac91c92697d299538bff68f9e93c60bc6
wadebooth <wademgmt@gmail.com>
2022-05-31 10:13:58 -0400

    created backend with database

--- src/gateway/mongo.ts
+++ src/gateway/mongo.ts
@@ -1,7 +1,7 @@
-import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb";
 
 export const getDb = async () => {
-        const client = new MongoClient(process.env.MONGO_URL!);
-        await client.connect();
-        return client.db('zachs-db-v2')
-    }
+  const client = new MongoClient(process.env.MONGO_URL!);
+  await client.connect();
+  return client.db("zachs-db-v2");
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import { createWish, getWishes } from './service/wish-list.service';
+import { config } from 'dotenv'
+
+config()
 
 const app = express();
 app.use(express.json());
